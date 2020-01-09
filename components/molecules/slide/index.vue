1b3284f045473d47e0e5a02449f461d874875ca3
Fripley <filipecheverrya@gmail.com>
2020-01-09 20:38:59 -0300

    add tag in product slider

--- components/molecules/slide/index.vue
+++ components/molecules/slide/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="slide-container">
-    <img :src="image" :alt="title" class="image-slide">
+    <span :class="['tag', verifyDiscount(percent)]">{{ `$ ${parseInt(Math.abs(percent))}` }}</span>
+    <figure class="image-wrapper">
+      <img :src="image" :alt="title" class="image-slide">
+    </figure>
     <Title :label="title" class="tertiary title-slide" />
     <span class="business-slide">
       Vendido por <strong>{{ business }}</strong>
@@ -39,6 +42,19 @@ export default {
       type: Number,
       default: 0
     }
+  },
+  methods: {
+    verifyDiscount (n) {
+      const res = Math.sign(n)
+      switch (res) {
+        case 1:
+          return 'positive'
+        case -1:
+          return 'negative'
+        default:
+          return 'neutral'
+      }
+    }
   }
 }
 </script>
@@ -70,6 +86,10 @@ export default {
   color: #3aacd1;
   font-weight: 600;
 }
+.image-wrapper {
+  height: 275px;
+  overflow: hidden;
+}
 .image-slide {
   width: 100%;
   max-width: 350px;
@@ -83,5 +103,39 @@ export default {
   color: #5555d9;
   font-weight: 600;
   font-size: 28px;
+}
+.tag {
+  position: absolute;
+  top: 24px;
+  left: 0;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  padding: 16px;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.tag:before {
+  content: '';
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  background: url(~assets/triangle-copy.png) 0 0 no-repeat;
+  background-size: contain;
+  margin-right: 8px;
+}
+.tag.negative {
+  background-color: #c1180f;
+}
+.tag.positive {
+  background-color: #13bc4a;
+}
+.tag.positive:before,
+.tag.neutral:before {
+  transform: rotate(180deg);
+}
+.tag.neutral {
+  background-color: #848484;
 }
 </style>
